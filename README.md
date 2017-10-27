8c112692dc785967075d3f030f18322e4e2a5de3
troido <troido@hotmail.com>
2017-10-27 22:51:42 +0200

    ground objects are now send sorted, and bugs are fixed. also, client doesn't crash anymore when resizing the window

--- README.md
+++ README.md
@@ -63,7 +63,6 @@ The idea is to make 3 different kind of areas:
 
 - better code documentation
 - testing
-- sort objects on ground on height
 - make idle NPC's stay around spawn
 - nondeterminism in combat and grow times
 - only plant seeds in soil
@@ -85,6 +84,7 @@ The idea is to make 3 different kind of areas:
 
 ## DONE
 
+- sort objects on ground on height
 - follow player when room larger than terminal
 - multiple actions per keypress
 - healthbar
--- server/ground.py
+++ server/ground.py
@@ -6,9 +6,8 @@ neighbourdirs = {"north":(0,-1), "south":(0,1), "east":(1,0), "west":(-1,0)}
 
 class GroundPatch:
     
-    def __init__(self, room, pos, sprite=' '):
+    def __init__(self, room, pos):
         self.objects = []
-        self.sprite = sprite
         self.room = room
         self.pos = pos
         self.neighbours = None
@@ -18,27 +17,31 @@ class GroundPatch:
         return not any(obj.isSolid() for obj in self.objects)
     
     def addObj(self, obj):
-        oldTop = self.getTopObj()
+        oldTop = self._getTopObj()
         self.objects.append(obj)
         self.objects.sort(key=(lambda o: -o.getHeight()))
-        if self.getTopObj() != oldTop:
-            self.event.trigger("changesprite", self.getPos(), obj.getSprite())
+        if self._getTopObj() != oldTop:
+            self.event.trigger("changesprite", self.getPos(), self.getTopSprite())
     
     def removeObj(self, obj):
+        oldTop = self._getTopObj()
         if obj in self.objects:
             self.objects.remove(obj)
-            topObj = self.getTopObj()
-            if obj.getHeight() >= topObj.getHeight():
-                self.event.trigger("changesprite", self.getPos(), topObj.getSprite())
+            if obj == oldTop:
+                self.event.trigger("changesprite", self.getPos(), self.getTopSprite())
+    
+    def getTopSprite(self):
+        topObj = self._getTopObj()
+        if topObj:
+            return topObj.getSprite()
+        else:
+            return ' '
     
     def getObjs(self):
         return tuple(self.objects)
     
-    def getTopObj(self):
+    def _getTopObj(self):
         return self.objects[0] if len(self.objects) else None
-    
-    def getSprite(self):
-        return self.sprite
     
     def onEnter(self, obj):
         for o in frozenset(self.objects):
@@ -59,9 +62,6 @@ class GroundPatch:
     
     def getPos(self):
         return self.pos
-    
-    def getHeight(self):
-        return -1
     
     
     def addListener(self, callback, key=None):
