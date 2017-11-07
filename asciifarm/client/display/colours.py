60d14512e20255b5aa11d7e0c3ce223452ead9e9
troido <troido@hotmail.com>
2017-11-07 10:53:00 +0100

    changed water colours and set default to 7,0

--- asciifarm/client/display/colours.py
+++ asciifarm/client/display/colours.py
@@ -13,6 +13,9 @@ class Colours:
             curses.init_pair(i, i%self.colours, i//self.colours)
     
     def get(self, fg=0, bg=0):
-        fg %= self.colours
-        bg %= self.colours
-        return curses.color_pair(fg + bg*self.colours)
+        dfg = fg % self.colours
+        dbg = bg % self.colours
+        if (dfg, dbg) == (0, 0) and (fg, bg) != (0, 0):
+            # avoid unintended use of (0,0), which is settings dependent
+            dfg, dbg = 7, 0
+        return curses.color_pair(dfg + dbg*self.colours)
