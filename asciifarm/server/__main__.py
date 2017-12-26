e7e53c565d6b9d5b26df61ab3c9186e4339c75d1
troido <troido@hotmail.com>
2017-12-27 00:23:43 +0100

    server now ensures that it's not ran as standalone package

--- asciifarm/server/__main__.py
+++ asciifarm/server/__main__.py
@@ -8,7 +8,7 @@ if sys.version_info[0] < 3:
     print("This game is written in python 3.\nRun 'python3 "+sys.argv[0]+"' or './"+sys.argv[0]+"'")
     sys.exit(-1)
 
-if __package__:
+if __package__ == "asciifarm.server":
     from . import main
 else:
     import os.path
