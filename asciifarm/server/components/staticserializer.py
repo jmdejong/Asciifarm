061b615db65dd280e7608655e9f7dbb46dc3b8e0
troido <troido@hotmail.com>
2018-01-18 18:08:26 +0100

    improved serialization again

--- asciifarm/server/components/staticserializer.py
+++ asciifarm/server/components/staticserializer.py
@@ -2,15 +2,18 @@
 
 from .component import Component
 
-class Serialize(Component):
+class StaticSerializer(Component):
     """ This class is for static serialization.
     
     If an entity has this component then the serialization will be the argument passed to the constructor instead of the toJSON of the entity and its components.
     This is useful for objects that hold no state and are not unique.
     """
     
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, data, *args, **kwargs):
+        if isinstance(data, str):
+            self.data = {"type": data, "args": args, "kwargs": kwargs}
+        else:
+            self.data = data
     
     def serialize(self):
         return self.data
--- asciifarm/server/gameobjects.py
+++ asciifarm/server/gameobjects.py
@@ -20,53 +20,53 @@ from .components.food import Food
 from .components.equippable import Equippable
 from .components.volatile import Volatile
 from .components.change import Change
-from .components.serialize import Serialize
+from .components.staticserializer import StaticSerializer as Static
 
 """ This module contains factory functions for many placable entities, and a make function to call a factory by a string name """
 
 entities = {}
 
-entities["wall"] = lambda: Entity(sprite="wall", height=2, flags={"solid"}, components={"serialize": Serialize("wall")})
+entities["wall"] = lambda: Entity(sprite="wall", height=2, flags={"solid"}, components={"serialize": Static("wall")})
 
-entities["freeland"] = lambda: Entity(name="buildable", flags={"freeland"}, components={"serialize": Serialize("freeland")})
+entities["freeland"] = lambda: Entity(name="buildable", flags={"freeland"}, components={"serialize": Static("freeland")})
 
-entities["rock"] = lambda: Entity(sprite="rock", height=10, flags={"solid"}, components={"serialize": Serialize("rock")})
+entities["rock"] = lambda: Entity(sprite="rock", height=10, flags={"solid"}, components={"serialize": Static("rock")})
 
-entities["tree"] = lambda: Entity(sprite="tree", height=3, flags={"solid"}, components={"serialize": Serialize("tree")})
+entities["tree"] = lambda: Entity(sprite="tree", height=3, flags={"solid"}, components={"serialize": Static("tree")})
 
-entities["house"] = lambda: Entity(sprite="house", height=3, flags={"solid"}, components={"serialize": Serialize("house")})
+entities["house"] = lambda: Entity(sprite="house", height=3, flags={"solid"}, components={"serialize": Static("house")})
 
-entities["fence"] = lambda: Entity(sprite="fence", height=1, flags={"solid"}, components={"serialize": Serialize("fence")})
+entities["fence"] = lambda: Entity(sprite="fence", height=1, flags={"solid"}, components={"serialize": Static("fence")})
 
-entities["stone"] = lambda: Entity(sprite="stone", height=0.4, components={"item": Build("builtwall", flagsNeeded={"freeland"}, blockingFlags={"solid", "occupied"}), "serialize": Serialize("stone")})
+entities["stone"] = lambda: Entity(sprite="stone", height=0.4, components={"item": Build("builtwall", flagsNeeded={"freeland"}, blockingFlags={"solid", "occupied"}), "serialize": Static("stone")})
 
-entities["pebble"] = lambda: Entity(sprite="pebble", height=0.2, components={"item": Item(), "serialize": Serialize("pebble")})
+entities["pebble"] = lambda: Entity(sprite="pebble", height=0.2, components={"item": Item(), "serialize": Static("pebble")})
 
 entities["grass"] = lambda: Entity(
-    sprite=random.choice(["ground"] + 2*["grass1", "grass2", "grass3"]), name="grass", height=0.1, flags={"floor", "soil"}, components={"serialize": Serialize("grass")})
+    sprite=random.choice(["ground"] + 2*["grass1", "grass2", "grass3"]), name="grass", height=0.1, flags={"floor", "soil"}, components={"serialize": Static("grass")})
 
 entities["greengrass"] = lambda: Entity(
-    sprite=random.choice(["grass1", "grass2", "grass3"]), height=0.1, flags={"floor", "soil"}, components={"serialize": Serialize("greengrass")})
+    sprite=random.choice(["grass1", "grass2", "grass3"]), height=0.1, flags={"floor", "soil"}, components={"serialize": Static("greengrass")})
 
-entities["floor"] = lambda: Entity(sprite="floor", height=0.1, flags={"floor"}, components={"serialize": Serialize("floor")})
+entities["floor"] = lambda: Entity(sprite="floor", height=0.1, flags={"floor"}, components={"serialize": Static("floor")})
 
-entities["ground"] = lambda: Entity(sprite="ground", height=0.1, flags={"floor", "soil"}, components={"serialize": Serialize("ground")})
+entities["ground"] = lambda: Entity(sprite="ground", height=0.1, flags={"floor", "soil"}, components={"serialize": Static("ground")})
 
-entities["bridge"] = lambda small=False: Entity(sprite=("smallbridge" if small else "bridge"), height=0.1, flags={"floor"}, components={"serialize": Serialize("bridge")})
+entities["bridge"] = lambda small=False: Entity(sprite=("smallbridge" if small else "bridge"), height=0.1, flags={"floor"}, components={"serialize": Static("bridge", small)})
 
-entities["water"] = lambda: Entity(sprite="water", height=0, components={"serialize": Serialize("water")})
+entities["water"] = lambda: Entity(sprite="water", height=0, components={"serialize": Static("water")})
 
 entities["roomexit"] = lambda destRoom, destPos=None, mask=(False, False), sprite=" ", size=0: Entity(
     sprite=sprite, height=size, components={"collision": Portal(destRoom, destPos, mask)})
 
 entities["rabbit"] = lambda: Entity(
-    sprite="rabbit", name="bunny", height=1, components={"move": Move(slowness=4), "controller": RandomWalkController(moveChance=0.05), "serialize": Serialize("rabbit")})
+    sprite="rabbit", name="bunny", height=1, components={"move": Move(slowness=4), "controller": RandomWalkController(moveChance=0.05), "serialize": Static("rabbit")})
 
 entities["dummy"] = lambda: Entity(
     sprite="dummy", height=1, flags={"occupied"}, components={"fighter": Fighter(maxHealth=20, strength=0), "alignment": Alignment(faction.NONE)})
 
 entities["spiketrap"] = lambda: Entity(
-    sprite="spikes", height=1, flags={"occupied"}, components={"fighter": Fighter(maxHealth=25, strength=25), "collision": Trap(), "serialize": Serialize("spiketrap")})
+    sprite="spikes", height=1, flags={"occupied"}, components={"fighter": Fighter(maxHealth=25, strength=25), "collision": Trap(), "serialize": Static("spiketrap")})
 
 entities["goblin"] = lambda home=None: Entity(sprite="goblin", height=1.2, components={
         "move": Move(slowness=3),
@@ -92,9 +92,10 @@ entities["rat"] = lambda home=None: Entity(sprite="rat", height=1, components={
         "loot": Loot([("seed", 0.9), ("seed", 0.3)])
         })
 
-entities["spawner"] = lambda objType, number, delay, sprite=None, name=None, height=0, setHome=False, initialSpawn=False, objArgs=None, objKwargs=None: Entity(
+entities["spawner"] = lambda objType, number, delay, sprite=None, name=None, height=0, setHome=False, initialSpawn=True, objArgs=None, objKwargs=None: Entity(
     sprite=sprite, height=height, name=name, components={
-        "spawn": Spawner(objType, number, delay, setHome, initialSpawn, objArgs, objKwargs)})
+        "spawn": Spawner(objType, number, delay, setHome, initialSpawn, objArgs, objKwargs),
+        "serialize": Static("spawner", objType, number, delay, sprite, name, height, setHome, initialSpawn, objArgs, objKwargs)})
 
 entities["sownseed"] = lambda: Entity(sprite="seed", height=0.05, name="plantedseed", flags={"occupied"}, components={"grow": Growing("youngplant", 100)})
 
@@ -103,12 +104,12 @@ entities["youngplant"] = lambda: Entity(sprite="youngplant", height=0.5, flags={
 entities["plant"] = lambda: Entity(sprite="plant", height=1.2, flags={"occupied"}, components={
         "interact": Harvest(),
         "loot": Loot([("seed", .92), ("seed", .20), ("food", .8), ("food", .4)]),
-        "serialize": Serialize("plant")
+        "serialize": Static("plant")
         })
 
-entities["food"] = lambda: Entity(sprite="food", height=0.3, components={"item": Food(20), "serialize": Serialize("food")})
+entities["food"] = lambda: Entity(sprite="food", height=0.3, components={"item": Food(20), "serialize": Static("food")})
 
-entities["seed"] = lambda: Entity(sprite="seed", height=0.2, components={"item": Build("sownseed", flagsNeeded={"soil"}, blockingFlags={"occupied", "solid"}), "serialize": Serialize("seed")})
+entities["seed"] = lambda: Entity(sprite="seed", height=0.2, components={"item": Build("sownseed", flagsNeeded={"soil"}, blockingFlags={"occupied", "solid"}), "serialize": Static("seed")})
 
 entities["builtwall"] = lambda: Entity(
     sprite="wall", height=2, flags={"solid"}, components={
@@ -116,33 +117,33 @@ entities["builtwall"] = lambda: Entity(
             "alignment": Alignment(faction.NONE),
             "loot": Loot([("stone", 1)])})
 
-entities["sword"] = lambda: Entity(sprite="sword", height=0.5, components={"item": Equippable("hand", {"strength": 5}), "serialize": Serialize("sword")})
+entities["sword"] = lambda: Entity(sprite="sword", height=0.5, components={"item": Equippable("hand", {"strength": 5}), "serialize": Static("sword")})
 
-entities["club"] = lambda: Entity(sprite="club", height=0.5, components={"item": Equippable("hand", {"strength": 3}), "serialize": Serialize("club")})
+entities["club"] = lambda: Entity(sprite="club", height=0.5, components={"item": Equippable("hand", {"strength": 3}), "serialize": Static("club")})
 
 entities["weapon"] = lambda strength=0, name="weapon": Entity(sprite="sword", name=name, height=0.5, components={"item": Equippable("hand", {"strength": strength})})
 
-entities["armour"] = lambda: Entity(sprite="armour", height=0.5, components={"item": Equippable("body", {"defense": 100}), "serialize": Serialize("armour")})
+entities["armour"] = lambda: Entity(sprite="armour", height=0.5, components={"item": Equippable("body", {"defense": 100}), "serialize": Static("armour")})
 
-entities["wound"] = lambda duration=4, height=0.2: Entity(sprite="wound", height=height, components={"volatile": Volatile(duration), "serialize": Serialize(None)})
+entities["wound"] = lambda duration=4, height=0.2: Entity(sprite="wound", height=height, components={"volatile": Volatile(duration), "serialize": Static(None)})
 
-entities["closeddoor"] = lambda: Entity(sprite="closeddoor", name="door", height=2, flags={"solid"}, components={"interact": Change("opendoor"), "serialize": Serialize("closeddoor")})
+entities["closeddoor"] = lambda: Entity(sprite="closeddoor", name="door", height=2, flags={"solid"}, components={"interact": Change("opendoor"), "serialize": Static("closeddoor")})
 
-entities["opendoor"] = lambda: Entity(sprite="opendoor", name="door", height=1, flags={"occupied"}, components={"interact": Change("closeddoor"), "serialize": Serialize("opendoor")})
+entities["opendoor"] = lambda: Entity(sprite="opendoor", name="door", height=1, flags={"occupied"}, components={"interact": Change("closeddoor"), "serialize": Static("opendoor")})
 
-entities["builtcloseddoor"] = lambda: Entity(sprite="closeddoor", name="door", height=2, flags={"solid"}, components={
+entities["builtcloseddoor"] = lambda health=None: Entity(sprite="closeddoor", name="door", height=2, flags={"solid"}, components={
     "interact": Change("builtopendoor"),
-    "fighter": Fighter(maxHealth=100, strength=0),
+    "fighter": Fighter(maxHealth=100, health=health, strength=0),
     "alignment": Alignment(faction.NONE),
     "loot": Loot([("hardwood", 1)])})
 
-entities["builtopendoor"] = lambda: Entity(sprite="opendoor", name="door", height=1, flags={"occupied"}, components={
+entities["builtopendoor"] = lambda health=None: Entity(sprite="opendoor", name="door", height=1, flags={"occupied"}, components={
     "interact": Change("builtcloseddoor"),
-    "fighter": Fighter(maxHealth=100, strength=0),
+    "fighter": Fighter(maxHealth=100, health=health, strength=0),
     "alignment": Alignment(faction.NONE),
     "loot": Loot([("hardwood", 1)])})
 
-entities["hardwood"] = lambda: Entity(sprite="hardwood", height=0.4, components={"item": Build("builtcloseddoor", flagsNeeded={"freeland"}, blockingFlags={"solid", "occupied"}), "serialize": Serialize("hardwood")})
+entities["hardwood"] = lambda: Entity(sprite="hardwood", height=0.4, components={"item": Build("builtcloseddoor", flagsNeeded={"freeland"}, blockingFlags={"solid", "occupied"}), "serialize": Static("hardwood")})
 
 def makeEntity(entType, roomData, *args, preserve=False, **kwargs):
     entity = entities[entType](*args, **kwargs)
