e87603a115f89175d5ae14f0f822dc14a4270161
troido <troido@hotmail.com>
2018-02-08 15:35:09 +0100

    stamps and alarms are now used in 'grow' too

--- asciifarm/server/components/grow.py
+++ asciifarm/server/components/grow.py
@@ -1,4 +1,4 @@
-from .. import timeout
+#from .. import timeout
 from .. import gameobjects
 import random
 from .component import Component
@@ -7,10 +7,15 @@ from .component import Component
 class Growing(Component):
     
     
-    def __init__(self, nextStage, duration, stepsPassed=None, nextArgs=None, nextKwargs=None):
+    def __init__(self, nextStage, duration=None, targetTime=None, nextArgs=None, nextKwargs=None, stepsPassed=None):
+        # stepsPassed is not used anymore, but included for backwards compatibility
+        
         self.nextStage = nextStage
-        self.duration = duration if stepsPassed is not None else int(random.triangular(duration/2, duration*2, duration))
-        self.stepsPassed = stepsPassed or 0
+        
+        # if both duration and targetTime are passed, duration is ignored
+        # if both are none, the growth will never happen
+        self.duration = duration
+        self.targetTime = targetTime
         self.nextArgs = nextArgs or []
         self.nextKwargs = nextKwargs or {}
     
@@ -21,9 +26,12 @@ class Growing(Component):
     
     def roomJoin(self, o, roomData):
         self.roomData = roomData
-        self.timeout = timeout.Timeout(roomData.getEvent("update"), self.duration - self.stepsPassed, self.grow)
+        if not self.targetTime and self.duration:
+            duration = int(random.triangular(self.duration/2, self.duration*2, self.duration))
+            self.targetTime = roomData.getStamp() + duration
+        self.roomData.setAlarm(self.targetTime, self.grow)
     
-    def grow(self, to):
+    def grow(self):
         
         obj = gameobjects.makeEntity(self.nextStage, self.roomData, *self.nextArgs, preserve=self.owner.isPreserved(), **self.nextKwargs)
         obj.place(self.owner.getGround())
@@ -33,15 +41,13 @@ class Growing(Component):
         
         self.owner.remove()
     
-    def remove(self):
-        self.timeout.remove()
     
     
     def toJSON(self):
         return {
             "nextStage": self.nextStage,
             "duration": self.duration,
-            "stepsPassed": self.stepsPassed,
+            "targetTime": self.targetTime,
             "nextArgs": self.nextArgs,
             "nextKwargs": self.nextKwargs
         }
