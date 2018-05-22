7e685068e40fdea2f1cea8317756feecb9995b30
troido <troido@protonmail.com>
2018-05-22 19:09:47 +0200

    client raises exceptions outside main curses wrapper

--- asciifarm/client/start.py
+++ asciifarm/client/start.py
@@ -23,6 +23,7 @@ def main(name, socketType, address, keybindings, characters, colours=False, logf
         print("ERROR: Could not connect to server.\nAre you sure that the server is running and that you're connecting to the right address?", file=sys.stderr)
         return
     
+    error = None
     closeMessage = None
     
     os.environ.setdefault("ESCDELAY", "25")
@@ -34,10 +35,18 @@ def main(name, socketType, address, keybindings, characters, colours=False, logf
             client.start()
         except KeyboardInterrupt:
             client.close("^C caught, goodbye")
+        except Exception as e:
+            # throw the execption outside ncurses
+            # so the cleanup can happen first
+            nonlocal error
+            error = e
         nonlocal closeMessage
         closeMessage = client.closeMessage
-    
+
     curses.wrapper(start)
+    
+    if error is not None:
+        raise error
     
     if closeMessage:
         print(closeMessage, file=sys.stderr)
