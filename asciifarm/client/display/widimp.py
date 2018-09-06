ae6dbfbfb771ef2902985a8828f4759a29a3b01d
troido <troido@protonmail.com>
2018-09-06 15:13:37 +0200

    actually include the superclass

---
+++ asciifarm/client/display/widimp.py
@@ -0,0 +1,14 @@
+
+
+class WidImp:
+    
+    """widget implementation"""
+    
+    def setWidget(self, widget):
+        self._widget = widget
+    
+    def change(self):
+        self._widget.change()
+    
+    def update(self, win):
+        pass
