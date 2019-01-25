c0d0d6acb2fde110edfa1766fe427e997d1b71c5
troido <troido@protonmail.com>
2019-01-25 10:28:23 +0100

    fixed list of default charmaps in help

--- asciifarm/client/main.py
+++ asciifarm/client/main.py
@@ -34,7 +34,7 @@ def main(argv=None):
     parser.add_argument("-a", "--address", help="The address of the socket. When the socket type is 'abstract' this is just a name. When it is 'unix' this is a filename. When it is 'inet' is should be in the format 'address:port', eg 'localhost:8080'. Defaults depends on the socket type")
     parser.add_argument("-s", "--socket", help="the socket type. 'unix' is unix domain sockets, 'abstract' is abstract unix domain sockets and 'inet' is inet sockets. ", choices=["abstract", "unix", "inet"], default="abstract")
     parser.add_argument('-k', '--keybindings', help='The file with the keybinding configuration. This file is a JSON file.', default="default")
-    parser.add_argument('-c', '--characters', help='The file with the character mappings for the graphics. If it is either of these names: {} it will be loaded from the charmaps directory.'.format(list(loaders.standardCharFiles.items())), default="default")
+    parser.add_argument('-c', '--characters', help='The file with the character mappings for the graphics. If it is either of these names: {} it will be loaded from the charmaps directory.'.format(list(loaders.standardCharFiles.keys())), default="default")
     parser.add_argument('-o', '--logfile', help='All game messages will be written to this file.', default=None)
     
     colourGroup = parser.add_mutually_exclusive_group()
