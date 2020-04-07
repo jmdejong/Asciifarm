752c8859aac330504b50b384f052ae057e195370
troido <troido@protonmail.com>
2020-04-07 09:59:26 +0200

    show equipment within the inventory

--- asciifarm/server/view.py
+++ asciifarm/server/view.py
@@ -15,8 +15,10 @@ def onSelectionChange(p):
 
 changeActions = {
     "health": lambda p: ["health", p.getHealthPair()],
-    "inventory": lambda p: ["inventory", [obj.getName() for obj in p.getInventory()]],
-    "equipment": lambda p: ["equipment", sorted([(slot, (item.getName() if item else None)) for slot, item in p.getEquipment().items()])],
+    "inventory": lambda p: 
+        ["inventory", 
+            [obj.getName() for obj in p.getInventory()] 
+            + sorted([slot + ":" + (item.getName() if item else "") for slot, item in p.getEquipment().items()])],
     "ground": lambda p: ["ground", [obj.getName() for obj in p.getGroundObjs() if obj.getName()]],
     "pos": lambda p: ["playerpos", p.getPos()],
     "selection": onSelectionChange
